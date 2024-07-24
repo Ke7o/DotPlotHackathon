b16f6fa97ae13e82dfa8999aef78e5a46d35d4f9
ke7o <jhtconner@outlook.com>
2024-07-24 15:09:48 +0100

    First commit, sqlite3 database created

---
+++ DotPlot/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Patients(models.Model):
+    patientID = models.IntegerField(primary_key=True)
+    patientName = models.CharField(max_length=100)
+    age = models.IntegerField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    history = models.BooleanField(default=False)
+    scanID = models.IntegerField()
+
+    def __str__(self):
+        return self.patientname
+
+
+class USscans(models.Model):
+    scanID = models.IntegerField(primary_key=True)
+    coordinates = models.CharField(max_length=2)
+    scanDate = models.DateField()
+    diagnosis = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"Scan ID: {self.scan_id}, Patient: {self.patient.patient_name}"
+
+
