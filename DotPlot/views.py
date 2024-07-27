fa439633a339b76b18fbbad7ad2af66fd8fc8716
ke7o <jhtconner@outlook.com>
2024-07-27 21:38:14 +0100

    database can now be searched using patient ID's

--- DotPlot/views.py
+++ DotPlot/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Patients
 
-# Create your views here.
+def patientList (request):
+    query = request.GET.get('q')
+
+    if query:
+        patients = Patients.objects.filter(patientID=query)
+    
+    else:
+        patients = Patients.objects.all()
+    
+    return render(request, 'patients_list.html', {'patients': patients})
